c1dcc365c622afaa9c539a4f6f019567f4aac9c7
Limoo Kiplimo <limoodrice@gmail.com>
2023-05-02 00:23:50 +0300

    Arrays and hashing

---
+++ Arrays/duplicates.py
@@ -0,0 +1,20 @@
+'''
+    Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+    
+'''
+
+# Solution:
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        elems = set()
+        for elem in nums:
+            if elem in elems:
+                return True
+            elems.add(elem)
+        return False
+    
+'''
+    Time complexity is O(1) since sets have a contant-time lookup.
+    Incase there are no duplicates (our worset case scenario), the space complexity is O(n).
+'''
